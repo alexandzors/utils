a4f782509df2514341fd222f74387bda6e65343d
Alexander Henderson <alexandzors@gmail.com>
2024-06-11 22:56:52 -0400

    Update linode.sh

--- linode-scripts/linode.sh
+++ linode-scripts/linode.sh
@@ -8,8 +8,9 @@ linode-cli firewalls rules-update \
     $LINODE_FW_ID
 ;;
 d)
-linode-cli firewalls rules-update $LINODE_FW_ID \
-    --inbound '[{"action":"ACCEPT", "protocol": "TCP", "ports": "22", "addresses": {"ipv4": ["'$HOME_IP'"]}, "label": "accept-inbound-SSH"}, {"action":"ACCEPT", "protocol": "TCP", "ports": "80", "addresses": {"ipv4": ["0.0.0.0/0"], "ipv6": ["::/0"]}, "label": "accept-inbound-HTTP"}, {"action":"ACCEPT", "protocol": "TCP", "ports": "443", "addresses": {"ipv4": ["0.0.0.0/0"], "ipv6": ["::/0"]}, "label": "accept-inbound-HTTPS"}]'
+linode-cli firewalls rules-update \
+    --inbound '[{"action":"ACCEPT", "protocol": "TCP", "ports": "22", "addresses": {"ipv4": ["'$HOME_IP'"]}, "label": "accept-inbound-SSH"}, {"action":"ACCEPT", "protocol": "TCP", "ports": "80", "addresses": {"ipv4": ["0.0.0.0/0"], "ipv6": ["::/0"]}, "label": "accept-inbound-HTTP"}, {"action":"ACCEPT", "protocol": "TCP", "ports": "443", "addresses": {"ipv4": ["0.0.0.0/0"], "ipv6": ["::/0"]}, "label": "accept-inbound-HTTPS"}]' \
+    $LINODE_FW_ID
 ;;
 esac
 done
